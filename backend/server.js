81d43ee1d04d755ac9712a9eeb1e75e0949007ad
Ashish <ashsihbhalray865@gmail.com>
2022-08-17 16:39:33 +0530

    changes in admin panel

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,6 @@ const app =require('./app')
 const {connectDatabase}=require('./config/database')
 require('dotenv').config({path:'backend/config/config.env'})
 connectDatabase();
-app.listen(process.env.PORT,()=>{
-    console.log(`Server running on port ${process.env.PORT}`)
+app.listen(3000,()=>{
+    console.log(`Server running on port `)
 })
